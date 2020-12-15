31cd7d087fcb044601654f4d56cd4a976329b3f2
jo3968 <sayo1z2@naver.com>
2020-12-16 01:20:56 +0900

    commit2

--- config/settings.py
+++ config/settings.py
@@ -85,14 +85,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'wehuddling',
+#         'USER': 'wehuddling',
+#         'PASSWORD': 'wehuddling',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wehuddling',
-        'USER': 'wehuddling',
-        'PASSWORD': 'wehuddling',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
